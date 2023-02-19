f9c73c9292214c9adb6017d649889a8860bd6eb6
sundaray <pgcim14.hemant@spjimr.org>
2023-02-19 19:57:15 +0530

    working on storage

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -45,49 +45,22 @@ export const uploadImages = async (
   setError,
   setSuccess
 ) => {
-  setLoading(true);
   const storeImage = async (image) => {
-    return new Promise((resolve, reject) => {
-      const storageRef = ref(storage, "images/" + image.name);
+    const storageRef = ref(storage, "images/" + image.name);
+    const uploadTask = uploadBytesResumable(storageRef, image);
 
-      const uploadTask = uploadBytesResumable(storageRef, image);
-
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          switch (snapshot.state) {
-            case "paused":
-              console.log("Upload is paused");
-              break;
-            case "running":
-              console.log("Upload is running");
-              break;
-          }
-        },
-        (error) => {
-          reject(error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            resolve(downloadURL);
-          });
-        }
-      );
+    uploadTask.on("state_changed", () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        return downloadURL;
+      });
     });
   };
 
   const imageUrls = await Promise.all(
-    images.map(async (image) => {
-      return await storeImage(image);
+    images.map((image) => {
+      storeImage(image);
     })
-  ).catch((error) => {
-    setLoading(false);
-    setSuccess(false);
-    setError(error.message);
-  });
+  );
 
   return imageUrls;
 };
